c64cc9f6130c42116d76e8ec95ccb7500f4f76f1
lyashenko.ivan <ivan.lyashenko.it@gmail.com>
2024-09-09 17:41:50 +0300

    complete task-2

---
+++ script.js
@@ -0,0 +1,9 @@
+let number1 = Math.floor(Math.random() * 100);
+
+if (number1 % 2 === 0) {
+    console.log(`${number1} :Это четное число`)
+}
+
+if (number1 % 2 !== 0) {
+    console.log(`${number1} :Это нечетное число`)
+}
