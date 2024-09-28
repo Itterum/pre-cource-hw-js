862cf48d63ffce53f3d75c6526199e51efc1a401
lyashenko.ivan <ivan.lyashenko.it@gmail.com>
2024-09-28 18:12:07 +0300

    complete task-5 lesson-02

---
+++ lesson-02/05.js
@@ -0,0 +1,12 @@
+let passport = {
+    name: 'Petr',
+    surname: 'Petrov',
+};
+
+let passport_copy = {};
+Object.assign(passport_copy, passport);
+
+passport_copy.name = 'Ivan';
+
+console.log(passport_copy);
+console.log(passport);
